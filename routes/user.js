b4ddc4c8a1c142cf8dc74690ac24c03049fd3798
Reem <reemalrashed2@gmail.com>
2021-03-19 18:19:39 +0300

    added login routes

--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,10 @@
 const router = require('express').Router(),
 UserController = require('../controllers/user')
 
+router.post('/login', UserController.authenticate)
+router.use(UserController.verifyJWT)
 router.get('/',UserController.index)
 router.get('/:uid',UserController.show)
-router.put('/:uid/update',UserController.update)
+router.put('/:uid/update', UserController.update)
 router.delete('/:uid/delete', UserController.delete)
-router.post('/create', UserController.create)
 module.exports = router
-
