b86012508c3b0f250d0cbe9a9c381dd2db88069a
Reem <reemalrashed2@gmail.com>
2021-03-19 18:17:40 +0300

    added jwt functions

--- controllers/user.js
+++ controllers/user.js
@@ -1,25 +1,27 @@
 const User = require("../models/user")
 const passport = require('passport')
+const jsonWebToken = require("jsonwebtoken");
+const { json } = require("express");
 
 
 module.exports = {
     index:(req,res)=>{
         User.find({})
         .then(users=>{
-            res.json(users)
+            res.json({users})
         })
         .catch(error=>{
-            res.json(error)
+            res.json({error: error})
         })
-    },
+    }, 
     show:(req,res)=>{
         let userId = req.params.uid
         User.findById(userId)
-        .then(user=>{
-            res.json(user)
+        .then(user =>{
+            res.json({user})
         })
-        .catch(error=>{
-            res.json(error)
+        .catch(error =>{
+            res.json({error: error})
         })
     },
     update:(req,res)=>{
@@ -31,41 +33,79 @@ module.exports = {
         }
         User.findByIdAndUpdate(userId,{$set: userInfo})
         .then(user=>{
-            res.json("User information has been updated")
+            res.json({message: "User information has been updated"})
         })
-        .catch(error =>{
-            res.json(error)
+        .catch(error=>{
+            res.json({error: error})
         })
     },
-    delete:(req,res)=>{
+    delete: (req,res)=>{
         let userId = req.params.uid
         User.findByIdAndRemove(userId)
-        .then(user =>{
-            res.json("User is deleted successfully")
+        .then( ()=>{
+            res.json({message: "User is deleted"})
         })
         .catch(error =>{
-            res.json(error)
+            res.json({error: error})
         })
     },
-    create:(req,res)=>{
-        let newUser = new User({
-            name: req.body.name,
-            age: req.body.age,
-            email: req.body.email
+    create: (req,res)=>{
+        let newUser= new User({
+            name : req.body.name ,
+            email : req.body.email ,
+            age : req.body.age
         })
-        User.register(newUser, req.body.password, (error, user)=> {
+        User.register(newUser, req.body.password, (error, user)=>{
             if(user){
-                res.json("User is inserted successfully")
-            }
-            else{
-                res.json({error: error})
+                res.json({message : "User inserted!"})
+            } else {
+                res.json({error : error })
             }
         })
     },
-    login: (req,res)=>{
-        passport.authenticate('local',{
-            
-        })
+    authenticate: (req, res, next) =>{
+        passport.authenticate('local', (error, user) =>{
+            if(user){
+                let signedToken = jsonWebToken.sign({
+                    data: user._id,
+                    exp: new Date().setDate(new Date().getDate() + 1)
+                },'Lacorbi86');
+
+                res.json({
+                    success: true,
+                    token: signedToken
+                });
+            }else{
+                res.json({
+                    success: false,
+                    message: 'Could not authenticate user.'
+                });
+            }
+        })(req, res, next);
+    },
+    verifyJWT: (req,res, next)=>{
+        let token = req.body.token
+        if(token){
+            jsonWebToken.verify(token, 'Lacorbi86', (errors, payload)=>{
+                if(payload){
+                    User.findById(payload.data).then(user =>{
+                        if(user){
+                            next()
+                        }
+                        else{
+                            res.json({error: error})
+                        }
+                    })
+                }
+                else{
+                    res.json({message: "No user account found", error: true})
+                }
+            })
+            next()
+        }
+        else{
+            res.json({error: "Please provide a token!"})
+        }
     }
 
 }
